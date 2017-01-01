394b332856cfe059ccb95996991e3206c004b9e2
Rishabh Kumar <rishabhk3003@gmail.com>
2017-01-01 14:46:06 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+var app = express();
+
+/*MIDDLEWARES*/
+app.set('view engine', 'pug');
+app.use(express.static(__dirname+'/public'));
+app.use((req, res, next) => {
+    var now = new Date().toString()
+    console.log('Now: ', now)
+    // console.log(res)
+    next()
+})
+// app.use((req, res, next) => {
+//     res.render('maintenance.pug', {
+//         message: 'Site under Maintenance'
+//     })
+// })
+
+/*ROUTES*/
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Express</h1>')
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.pug', {
+        title: 'About Page',
+        heading: 'About Page',
+        message: 'Some text Here',
+    })
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to fetch the request!!'
+    })
+})
+
+/*PORT*/
+app.listen(3000, () => {
+    console.log('Server is up and running on port 3000...')
+});
